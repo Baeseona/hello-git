2ed59b6e1281c6835b16164f14ae8909b6027042
Baeseona <sunn0612@naver.com>
2023-11-23 08:00:21 +0000

    Sixth commit

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Frist commit!");
 		System.out.println("Fifth commit!");
+		System.out.println("Sixth commit!");
 	}
 }
 
